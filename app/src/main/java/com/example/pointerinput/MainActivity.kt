8afa9affddc139bcc1aaec1baec5a19fd4cf50e7
navneetpal01 <navneetpalgithub@gmail.com>
2024-03-11 01:20:58 +0530

    First Commit

--- app/src/main/java/com/example/pointerinput/MainActivity.kt
+++ app/src/main/java/com/example/pointerinput/MainActivity.kt
@@ -3,13 +3,35 @@ package com.example.pointerinput
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import com.example.pointerinput.ui.theme.PointerInputTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,14 +39,82 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PointerInputTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                    ImageVerticalGrid(feedItem = TestData.testItemsList)
             }
         }
     }
 }
 
+//Every kind of physical object Interaction with app called uses Pointer
+//1-Composable inbuilt support - onClick
+//2-gesture modifiers : Applied to composable -> Clickable , draggable , scrollable etc
+//3-Low level, using pointerInput -> detectTabGesture , detectDragGesture etc...
+
+
+@Composable
+fun ImageVerticalGrid(feedItem: List<TestDataItem>) {
+    var selectedItem by rememberSaveable {
+        mutableStateOf<TestDataItem?>(null)
+    }
+
+    var selectedImageItem by rememberSaveable {
+        mutableStateOf<TestDataItem?>(null)
+    }
+    LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+        items(items = TestData.testItemsList, key = {
+            it.id
+        }) { feed ->
+            Image(
+                painter = rememberAsyncImagePainter(model = feed.url),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(1.dp)
+                    .height(140.dp)
+                    .wrapContentWidth()
+                    .pointerInput(Unit) {
+                        //For capturing all Events
+                        awaitEachGesture {
+                            //Consuming means we are using it we are letting it happen
+                            awaitFirstDown().consume()
+                            waitForUpOrCancellation().let {
+                                it?.consume()
+                                selectedItem = feed
+                            }
+                        }
+                    }
+            )
+        }
+    }
+    if (selectedItem != null){
+        FullScreenImage(feedItem = selectedItem){
+            selectedItem = null
+        }
+    }
+}
+
+@Composable
+fun FullScreenImage(feedItem: TestDataItem?,onDismiss : () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.DarkGray.copy(alpha = 0.75f))
+                .clickable {
+                    onDismiss()
+                }
+        ) {
+        }
+        Image(
+            painter = rememberAsyncImagePainter(model = feedItem?.url),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+    }
+}
---
+++ app/src/main/java/com/example/pointerinput/TestData.kt
@@ -0,0 +1,33 @@
+package com.example.pointerinput
+
+import kotlin.random.Random
+
+object TestData {
+
+
+    val imagesList = listOf(
+        "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg",
+        "https://img.freepik.com/free-photo/beautiful-view-greenery-bridge-forest-perfect-background_181624-17827.jpg",
+        "https://img.freepik.com/free-photo/green-field-tree-blue-skygreat-as-backgroundweb-banner-generative-ai_1258-153069.jpg",
+        "https://img.freepik.com/free-photo/waterfall-nature-thailand_335224-989.jpg",
+        "https://img.freepik.com/free-photo/beautiful-shot-crystal-clear-lake-snowy-mountain-base-during-sunny-day_181624-5459.jpg",
+        "https://img.freepik.com/free-photo/aerial-view-koh-hong-island-krabi-thailand_335224-1378.jpg",
+    )
+
+    val testItemsList = List(44) {
+        val random = (0..5).random()
+        TestDataItem(
+            id = Random.nextInt(100, 100000).toString(),
+            url = imagesList[random],
+            title = "Title $it"
+        )
+    }
+
+}
+
+
+data class TestDataItem(
+    var id: String,
+    var title: String,
+    var url: String
+)
