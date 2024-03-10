066022883cbcf00321ae38be5602bbc55923b4fc
navneetpal01 <navneetpalgithub@gmail.com>
2024-03-10 20:28:08 +0530

    First Commit

---
+++ app/src/main/java/com/example/pointerinput/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.pointerinput
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pointerinput.ui.theme.PointerInputTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PointerInputTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
